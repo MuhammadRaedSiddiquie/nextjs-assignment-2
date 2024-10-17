7858444619e9a79020f4b5a1d2de8dfcdc42ce77
MuhammadRaedSiddiquie <bhairaed636@gmail.com>
2024-10-17 11:24:52 +0500

    charset remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>NextApp</title>
       </head>
